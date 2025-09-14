6e4ca9c46bce55297f4beccdcf04d9e35e6d1e87
karrad1201 <karad120109@gmail.com>
2025-09-14 19:47:02 +0500

    <3

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,16 @@
-import sys
 import asyncio
 import pytest
 import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 os.environ["TESTING"] = "1"
+
 from fastapi.testclient import TestClient
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from src.infrastructure.database.schemas import Base
 from src.main import app
-
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 TEST_DATABASE_URL = "sqlite+aiosqlite:///:memory:"
 engine = create_async_engine(TEST_DATABASE_URL, echo=False)
