247c1f3d4038dee87262446b4038ed7a33bc309d
karrad1201 <karad120109@gmail.com>
2025-09-25 22:39:03 +0500

    <3

--- src/main.py
+++ src/main.py
@@ -27,13 +27,14 @@ async def lifespan(app):
     finally:
         logger.info("Database connection closed")
 
+
 application = Application(lifespan=lifespan)
 app = application.app
 application.include_routers(routers)
 
-
 if __name__ == '__main__':
     import uvicorn
+
     try:
         uvicorn.run(app, host='0.0.0.0', port=8000)
     except KeyboardInterrupt:
